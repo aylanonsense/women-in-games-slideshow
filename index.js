029a71ad1ef275b1a5559c381d39a784e80adb8b
Ayla Myers <ayla@etsy.com>
2015-11-10 00:01:42 -0500

    First batch of usable slides

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var app = express();
 
 //serve web stuff
 app.use(express.static(__dirname + '/public'));
-/*app.get('/reveal.js', function(req, res) {
-	res.sendFile(__dirname + '/node_modules/requirejs/require.js');
-});*/
-app.use('/javascripts', express.static(__dirname + '/javascripts'));
 
 //run server
 app.listen(process.env.PORT || 3000);
